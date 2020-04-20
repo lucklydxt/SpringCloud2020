6c8e62ff8e50051c7a2788d4d1fba413420475e2
dxt <2335596051@qq.com>
2020-04-20 17:17:27 +0800

    单机版eureka 的部署和调试

--- cloud-eureka-server7001/src/main/java/com/zmt/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/zmt/springcloud/EurekaMain7001.java
@@ -2,16 +2,17 @@ package com.zmt.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author dxt
- * @date 2020/4/8 15:21
+ * @date 2020/4/20 16:26
  */
+
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class,args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
 }
--- cloud-provider-payment8001/src/main/java/com/zmt/springcloud/Payment8001.java
+++ cloud-provider-payment8001/src/main/java/com/zmt/springcloud/Payment8001.java
@@ -3,10 +3,12 @@ package com.zmt.springcloud;
 import com.sun.glass.ui.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 //初学参考文章https://blog.csdn.net/qq_29428213/article/details/104978389
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Payment8001 {
     public static void main(String[] args) {
         SpringApplication.run(Payment8001.class,args);
